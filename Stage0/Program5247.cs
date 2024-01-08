256f6f545799b1c0cd67943cc93113e15837ffcc
esterdrey <edray@g.jct.ac.il>
2024-01-08 17:13:53 +0200

    stage0

---
+++ Stage0/Program5247.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Stage0
+{
+    partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            Welcome1929();
+            Welcome5247();
+            Console.ReadKey();
+        }
+
+        private static void Welcome5247()
+        {
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0},Welcome to my first console application", name);
+        }
+        static partial void Welcome1929();
+        
+    }
+}
