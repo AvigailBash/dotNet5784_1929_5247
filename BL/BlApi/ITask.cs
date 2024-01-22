5de06cf4f05cefb958aa55e840db080149de3da3
Avigail Bash <abash@g.jct.ac.il>
2024-01-22 22:48:16 +0200

    interface

---
+++ BL/BlApi/ITask.cs
@@ -0,0 +1,10 @@
+﻿namespace BlApi;
+
+public interface ITask
+{
+    public IEnumerable<ITask> ReadAll(Func<BO.Task, bool>? filter = null);
+    public BO.Task Read(int id);
+    public void Create(BO.Task task);
+    public void Update(BO.Task task);
+    public void Delete(int id);
+}
