70a4f5c9770e111d31622dab29779d94e6f075ab
Avigail Bash <abash@g.jct.ac.il>
2024-01-25 19:19:35 +0200

    interface BL

---
+++ BL/BlImplementation/MilestoneInTaskImplementation.cs
@@ -0,0 +1,8 @@
+﻿using BlApi;
+
+namespace BlImplementation;
+
+internal class MilestoneInTaskImplementation: IMilestoneInTask
+{
+    private DalApi.IDal _dal = DalApi.Factory.Get;
+}
---
+++ BL/BlImplementation/TaskInListImplementation.cs
@@ -0,0 +1,8 @@
+﻿using BlApi;
+
+namespace BlImplementation;
+
+internal class TaskInListImplementation: ITaskInList
+{
+    private DalApi.IDal _dal = DalApi.Factory.Get;
+}
