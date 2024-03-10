709264aae6a948a77b0839b394452a6a74129c47
esterdrey <edray@g.jct.ac.il>
2024-03-11 00:03:07 +0200

    Stage6

--- PL/Converters.cs
+++ PL/Converters.cs
@@ -3,8 +3,33 @@ using System.Windows.Data;
 using System.Linq;
 using System.Text;
 using System.Windows.Media;
+using System.Windows.Controls;
+using System.Data;
 
 namespace PL;
+
+class DataGridRowBackGrounfConverter : IValueConverter
+{
+    static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+       DataGridRow dataGridRow = value as DataGridRow;
+        var data = dataGridRow.DataContext as DataRowView;
+        var taskId = int.Parse(data.Row.ItemArray[0]!.ToString()!);
+        var task = s_bl.Task.Read(taskId);
+        //if (dataGridRow.)
+        //{
+
+        //}
+        return task.completeDate is null ? new SolidColorBrush(Colors.Red) : new SolidColorBrush(Colors.Blue);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
 
 class ConvertIdToContent : IValueConverter
 {
@@ -47,23 +72,6 @@ class ConvertTextToUserId : IValueConverter
 
 }
 
-//public class StatusToColorConverter : IValueConverter
-//{
-//    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-//    {
-//        if (value is BO.Status status)
-//        {
-//            return colorMapping[status];
-//        }
-
-//        return Brushes.Transparent;
-//    }
-
-//    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-//    {
-//        throw new NotImplementedException();
-//    }
-//}
 
 class ConvertDaTteTimeToIsEnabled : IValueConverter
 {
@@ -83,9 +91,9 @@ class ConvertStatusToIsColor : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        if(((BO.Status) value )== BO.Status.Scheduled) { return Colors.BlueViolet; }
-        if(((BO.Status) value )== BO.Status.Done) { return Colors.Red; }
-        if(((BO.Status) value )== BO.Status.None) { return Colors.Green; }
+        if (((BO.Status)value) == BO.Status.Scheduled) { return Colors.BlueViolet; }
+        if (((BO.Status)value) == BO.Status.Done) { return Colors.Red; }
+        if (((BO.Status)value) == BO.Status.None) { return Colors.Green; }
         return Colors.White;
     }
 
@@ -95,3 +103,7 @@ class ConvertStatusToIsColor : IValueConverter
     }
 
 }
+
+
+
+
